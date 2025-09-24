eee47cecf8a238e3712fcb43e177221ec63f04cb
Your Name <tyler@tdstudiosny.com>
2025-09-24 01:12:16 -0400

    fix: Broaden Tailwind content paths to fix style loading

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./App.tsx",
-    "./index.tsx",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
